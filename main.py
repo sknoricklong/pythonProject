fe02212d14aad3651f2d71310335a6ede286bc08
sknoricklong <seankeenanlong@gmail.com>
2023-07-22 13:27:18 -0700

    Remove BigQuery

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     st.set_page_config(
@@ -16,8 +15,10 @@ if __name__ == '__main__':
         initial_sidebar_state="expanded"
     )
 
-    median_rating, median_workload = 4.453968253968255, 3.2888768115942053
+
     df_full = pd.read_csv("./fall_2023_courses_cleaned.csv")
+    median_rating = df_full['mean_rating'].median()
+    median_workload = df_full['mean_workload'].median()
 
     with st.sidebar:
         options = ["See all courses", "Search for courses"]
@@ -26,27 +27,34 @@ if __name__ == '__main__':
             selected = st.text_input("Add search terms, separated by comma:", "environment, climate, rosenbach")
             selected = [string.strip() for string in selected.split(',')]
 
-            df = df_full[df_full["course_description"].str.contains('|'.join(selected), case=False) |
+            df = df_full[df_full["description"].str.contains('|'.join(selected), case=False) |
                          df_full["course_name"].str.contains('|'.join(selected), case=False) |
+                         df_full["course_code"].str.contains('|'.join(selected), case=False) |
                          df_full["professor"].str.contains('|'.join(selected), case=False)]
         elif type == "See all courses":
             df = df_full.copy()
 
+
         # Term Selection
-        st.markdown("**Filter by Session:**")
-        full_term = st.checkbox("Full Term", value=True)
-        fall_1 = st.checkbox("Fall 1", value=True)
-        fall_2 = st.checkbox("Fall 2", value=True)
+        st.markdown("**Filter by Term:**")
+        unique_terms = df['term'].dropna().unique().tolist()
+        selected_terms = [term for term in unique_terms if st.checkbox(term, value=True, key=term)]
+
+        # Concentration Selection
+        st.markdown("**Filter by Concentration:**")
+        unique_concentrations = sorted(df['concentration'].dropna().unique().tolist())
+        selected_concentrations = [concentration for concentration in unique_concentrations if
+                                   st.checkbox(concentration, value=True, key=concentration)]
 
         st.markdown("")
         st.markdown("👉 [Feedback?](https://forms.gle/dVQtp7XwVhqnv5Dw8)")
 
-    if not fall_1:
-        df = df[df['session'] != 'Fall 1']
-    if not fall_2:
-        df = df[df['session'] != 'Fall 2']
-    if not full_term:
-        df = df[df['session'] != 'Full Term']
+        if selected_terms:
+            df = df[df['term'].isin(selected_terms)]
+
+        if selected_concentrations:
+            df = df[df['concentration'].isin(selected_concentrations)]
+
 
     def plot_scatterplot(df):
         fig = px.scatter(df, x='mean_rating', y='mean_workload', hover_name="course_name", hover_data=['prof_search', 'mean_rating', 'mean_workload'],
@@ -112,8 +120,8 @@ if __name__ == '__main__':
 
         fig.add_shape(type="rect",
                       xref="x", yref="y",
-                      x0=3.72, y0=4.30,
-                      x1=3.945, y1=4.45,
+                      x0=3.72, y0=4.175,
+                      x1=3.945, y1=4.325,
                       line=dict(
                           color="LightSeaGreen",
                           width=0,
@@ -138,8 +146,8 @@ if __name__ == '__main__':
 
         fig.add_shape(type="rect",
                       xref="x", yref="y",
-                      x0=4.79, y0=4.30,
-                      x1=5.015, y1=4.45,
+                      x0=4.79, y0=4.175,
+                      x1=5.015, y1=4.325,
                       line=dict(
                           color="LightSeaGreen",
                           width=0,
@@ -152,7 +160,7 @@ if __name__ == '__main__':
 
         fig.add_trace(go.Scatter(
             x=[4.9, 3.83, 3.83, 4.9],
-            y=[2.125, 4.375, 2.125, 4.375],
+            y=[2.125, 4.25, 2.125, 4.25],
             text=["Better prof, less effort",
                   "Worse prof, more effort",
                   "Worse prof, less effort",
@@ -161,7 +169,7 @@ if __name__ == '__main__':
         ))
 
         fig.update_xaxes(range=[3.7, 5.025])
-        fig.update_yaxes(range=[2, 4.5])
+        fig.update_yaxes(range=[2, 4.6])
         fig.update_layout(showlegend=False)
 
         fig.update_layout(uniformtext_minsize=15)
@@ -169,8 +177,8 @@ if __name__ == '__main__':
 
     st.header("🗓️ Compare Fall 2023 Courses at HKS")
     st.info("""
-    - See courses based on instructor **quality** (horizontal axis) and **workload** (vertical axis), using data from 3,000+ course evaluations on KNET 
-    - Each point represents the **average score** the professor has received across all courses taught
+    - See courses based on instructor **quality** (horizontal axis) and **workload** (vertical axis) 
+    - Each point represents the **average score** the professor has received across their 3 most recent courses taught
     - Explore **all** courses or search **specific** courses, then hover over the points to see course scores
     """)
     plot_scatterplot(df)
@@ -180,12 +188,14 @@ if __name__ == '__main__':
 
     st.header("🥇 Courses Ranked by  Professor's Average Rating")
     st.info("👉 Scroll right for course url on my.harvard.edu")
-    df_with_previous = df[['course_name', 'professor', 'mean_rating', 'mean_workload', 'session', 'days', 'time', 'course_id', 'course_description', 'url']].sort_values(by=['mean_rating', 'mean_workload'], ascending=[False, True]).reset_index(drop=True)
+    df_with_previous = df[['course_name', 'professor', 'mean_rating', 'mean_workload', 'term', 'day', 'time', 'course_code', 'description', 'course_link']].sort_values(by=['mean_rating', 'mean_workload'], ascending=[False, True]).reset_index(drop=True)
     st.write(df_with_previous.dropna(subset=['mean_rating']))
 
     st.header("📈 New Professors")
     st.info("👉 Scroll right for course url on my.harvard.edu")
-    st.write(df_with_previous[df_with_previous['mean_rating'].isna()].reset_index(drop=True))
+    df_new_professors = df_with_previous[df_with_previous['mean_rating'].isna()].reset_index(drop=True)
+    df_new_professors = df_new_professors.drop(columns=['mean_rating', 'mean_workload'])
+    st.write(df_new_professors)
 
     # for index, row in df.iterrows():
     #     st.markdown(f"**{index+1}. {row['course_name']}**")
